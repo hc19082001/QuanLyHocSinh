bf703b4b3e93d6c686cf33b63b7505f214c445de
minhle-1199 <lqm1908@gmail.com>
2021-12-16 22:32:31 +0700

    Xóa nút thêm trong MENU

--- app/src/main/java/com/example/quanlyhocsinh/MainActivityMENU.java
+++ app/src/main/java/com/example/quanlyhocsinh/MainActivityMENU.java
@@ -9,23 +9,15 @@ import android.widget.Button;
 
 public class MainActivityMENU extends AppCompatActivity {
 
-    Button btnThem;
+
     Button btnDanhsach;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        btnThem = findViewById(R.id.btnTHEM);
         btnDanhsach = findViewById(R.id.btnDANHSACH);
 
-        btnThem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivityMENU.this,MainActivityTHEM.class);
-                startActivity(intent);
-            }
-        });
 
         btnDanhsach.setOnClickListener(new View.OnClickListener() {
             @Override
